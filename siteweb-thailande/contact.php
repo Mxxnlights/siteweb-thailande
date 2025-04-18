414a2523d01228c4bdf1323bfe99c5115c19744d
Mxxnlights <nessym.mahdhi07@protonmail.com>
2025-04-18 16:54:33 +0200

    Add files via upload

---
+++ siteweb-thailande/contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="contact.css">
+    <title>Visiter la Thaïlande - Contact</title>
+    <link rel="icon" type="image/x-icon" href="icon.ico">
+
+</head>
+<body>
+<header>
+    <div class="navigation">
+        <nav>
+            <a class="" href="index.php">Home</a>
+            <a href="visite.php">Lieu à visiter</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+    </div>
+</header>
+</body>
+</html>
---
+++ siteweb-thailande/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css">
+    <title>Visiter la Thaïlande - Accueil</title>
+    <link rel="icon" type="image/x-icon" href="icon.ico">
+
+</head>
+<body>
+<header>
+    <div class="navigation">
+        <nav>
+            <a class="" href="index.php">Home</a>
+            <a href="visite.php">Lieu à visiter</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+    </div>
+
+</header>
+<!-- Pop-up de consentement des cookies -->
+<div id="cookie-consent-popup" class="cookie-consent">
+    <p>Ce site utilise des cookies pour vous garantir la meilleure expérience. <a href="cookie.php" target="_blank">En savoir plus</a></p>
+    <button id="accept-cookies">J'accepte</button>
+</div>
+<script src="index.js"></script>
+<div class="img-background">
+    <h1>Visiter la Thaïlande</h1>
+    <p>Découvrez les merveilles de la Thaïlande, de ses plages paradisiaques à ses temples anciens.</p>
+
+</div>
+</body>
+</html>
